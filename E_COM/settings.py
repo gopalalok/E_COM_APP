30e17ec255ae6a8cfa74f49e9ee14065ea4b31ba
Gopal Das <gd2088@cse.jgec.ac.in>
2021-04-13 16:43:01 +0530

    edit

--- E_COM/settings.py
+++ E_COM/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = "/image/download/"
 MEDIA_ROOT = BASE_DIR
 
