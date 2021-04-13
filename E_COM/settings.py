ad303e788a2a081bc6d84ae727542c60ddca8009
Gopal Das <gd2088@cse.jgec.ac.in>
2021-04-13 17:05:51 +0530

    edit

--- E_COM/settings.py
+++ E_COM/settings.py
@@ -124,10 +124,11 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),]
 MEDIA_URL = "/image/download/"
 MEDIA_ROOT = BASE_DIR
 
